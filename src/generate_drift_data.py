ff98591e7987d6b2f92575183918477b33ea85af
trieupham133-hash <trieupham133@gmail.com>
2026-05-18 19:35:02 +0700

    Chỉnh tỉ lệ nợ xấu trong dữ liệu tổng hợp để phù hợp với thực tế hơn.

--- src/generate_drift_data.py
+++ src/generate_drift_data.py
@@ -6,20 +6,23 @@ def generate_drifted_credit_data(n_samples=1000, random_state=100):
     # Cố định seed nhưng dùng số khác (100) để phân phối khác đi
     np.random.seed(random_state)
     
-    # 1. Thu nhập (~18 triệu, thấp hơn so với tập train)
-    mu = np.log(18000000) 
+    # 1. Thu nhập (~25 triệu, thấp hơn so với tập train)
+    mu = np.log(25000000) 
     thu_nhap = np.random.lognormal(mean=mu, sigma=0.4, size=n_samples)
-    thu_nhap = np.round(thu_nhap.clip(5000000, 100000000), -5)
+    thu_nhap = np.round(thu_nhap.clip(8000000, 200000000), -5)
     
-    # 2. Thời hạn vay ngắn hơn (Chỉ còn 6, 12, 18, 24 tháng)
-    thoi_han_vay = np.random.choice([6, 12, 18, 24], size=n_samples)
+    # 2. Thời hạn vay phân bố ở mức 1.5 - 2 năm (Ưu tiên vay ngắn hạn hơn so với tập train)
+    ky_han = [6, 12, 18, 24, 36, 48, 60]
+    ty_le_phan_bo = [0.05, 0.1, 0.3, 0.15, 0.2, 0.15, 0.05]   #Ưu tiên vay ngắn hạn
+    thoi_han_vay = np.random.choice(ky_han, size=n_samples, p = ty_le_phan_bo)
     
-    # 3. Số tiền vay (Gấp 15 - 25 lần thu nhập tháng)
-    he_so_vay = np.random.uniform(15, 25, n_samples)
+    # 3. Số tiền vay (Gấp 3 - 12 lần thu nhập tháng)
+    he_so_vay = np.random.uniform(3, 12, n_samples)
     so_tien_vay = np.round((thu_nhap * he_so_vay), -6).clip(10000000, 3000000000)
 
-    # 4. Điểm tín dụng (~420)
-    diem_tin_dung = 420 + np.random.normal(0, 60, n_samples)
+    # 4. Điểm tín dụng (~480)
+    thu_nhap_trieu = thu_nhap / 1000000
+    diem_tin_dung = 480 +  (thu_nhap_trieu * 0.5) + np.random.normal(0, 60, n_samples)
     diem_tin_dung = diem_tin_dung.clip(300, 850).astype(int)
 
     # 5. Tính lãi suất & Trả hàng tháng
@@ -34,7 +37,7 @@ def generate_drifted_credit_data(n_samples=1000, random_state=100):
     ty_le_tra_no = tra_hang_thang / thu_nhap
     diem_rui_ro = (ty_le_tra_no * 4) - (diem_tin_dung / 600) + np.random.normal(0, 0.5, n_samples)
 
-    xac_suat_no_xau = 1 / (1 + np.exp(-(diem_rui_ro - 1.5)))
+    xac_suat_no_xau = 1 / (1 + np.exp(-(diem_rui_ro - 1.5035)))
     lich_su_no_xau = np.random.binomial(1, xac_suat_no_xau)
 
     # Gom dữ liệu vào DataFrame
